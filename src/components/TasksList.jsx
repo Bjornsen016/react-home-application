db361229856719eb53d055328001d78f2473ee8a
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-09-13 17:08:53 +0200

    TasksList - WIP

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,19 +1,73 @@
-import React, { useState } from "react";
-import { List } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/fetcher";
+import { Tabs, Tab } from "@mui/material";
 
-const TasksList = () => {
-  const { taskList, setTaskList } = useState({});
-  const { task, setTask } = useState({});
+const TasksList = ({ token }) => {
+  const [value, setValue] = React.useState(0);
+  const [loading, setLoading] = useState(true);
+  const [taskLists, setTaskLists] = useState();
+  const [task, setTask] = useState();
 
-  const taskUrl = "https://Tasks.googleapis.com/tasks/v1/";
-  const getTaskListsUrl = "users/@me/lists";
-  const insertTaskUrl = `lists/${taskList}/tasks`;
-  const updateTaskUrl = `lists/${taskList}/tasks/${task}`;
-  const deleteTaskUrl = `lists/${taskList}/tasks/${task}`;
+  const taskUrl = new URL("https://content-tasks.googleapis.com");
+  const orginPath = "/tasks/v1";
+  const getTaskListsUrl = "/users/@me/lists";
+  const getListOfTasksUrl = `/lists/${taskLists}/tasks`;
+  const insertTaskUrl = `/lists/${taskLists}/tasks`;
+  const updateTaskUrl = `/lists/${taskLists}/tasks/${task}`;
+  const deleteTaskUrl = `/lists/${taskLists}/tasks/${task}`;
+
+  taskUrl.pathname = orginPath;
+
+  const getTaskList = async (urlPath) => {
+    taskUrl.pathname += urlPath;
+
+    const taskListData = await fetchDataFromApi(taskUrl, token).then(
+      setLoading(false).then(setList).then(getTasks)
+    );
+
+    taskUrl.pathname = orginPath;
+
+    const setList = async () => {
+      await setTaskLists(taskListData);
+    };
+
+    //setList().then(getTasks);
+
+    console.log(taskListData);
+  };
+
+  const getTasks = async (taskLists) => {
+    console.log(taskLists.items);
+
+    for (let i = 0; i < taskLists.items.length; i++) {
+      taskUrl.pathname += "/lists/" + taskLists.items[i].id + "/tasks";
+      const listOfTasksData = await fetchDataFromApi(taskUrl, token);
+
+      taskUrl.pathname = orginPath;
+
+      console.log(listOfTasksData);
+    }
+  };
+
+  useEffect(() => {
+    getTaskList(getTaskListsUrl);
+  }, []);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div>
-      <li>Lorem ipsum</li>
+      <Tabs value={value} onChange={handleChange}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          taskLists.items.map((list) => {
+            return <Tab label={list.title} />;
+          })
+        )}
+      </Tabs>
     </div>
   );
 };
--- src/components/routes/MainInformationScreen.jsx
+++ src/components/routes/MainInformationScreen.jsx
@@ -1,49 +1,46 @@
 import { Box, Button } from "@mui/material";
+import TasksList from "../TasksList";
 
 //TODO: Fix styling
 export default function MainInformationScreen({ user, googleApiToken }) {
-	return (
-		<>
-			<Box
-				borderColor='textPrimary'
-				sx={{
-					border: "2px solid",
-					gridColumn: "span 2",
-					padding: "10px",
-				}}
-			>
-				<Button onClick={() => console.log(user)}>Log user</Button>
-				Calender + other components here
-			</Box>
-			<Box
-				borderColor='textPrimary'
-				sx={{
-					border: "2px solid",
-					gridColumn: "span 1",
-					padding: "10px",
-				}}
-			>
-				{user !== undefined ? <div>Hej {user.names.givenName}</div> : ""}
-				<ul>
-					<li>TODOS</li>
-					<li>Buss tabell</li>
-					<li>Annat?</li>
-				</ul>
-			</Box>
-			<Box
-				borderColor='textPrimary'
-				sx={{
-					border: "2px solid",
-					gridColumn: "span 1",
-					padding: "10px",
-				}}
-			>
-				<ul>
-					<li>TODOS</li>
-					<li>Buss tabell</li>
-					<li>Annat?</li>
-				</ul>
-			</Box>
-		</>
-	);
+  return (
+    <>
+      <Box
+        borderColor="textPrimary"
+        sx={{
+          border: "2px solid",
+          gridColumn: "span 2",
+          padding: "10px",
+        }}
+      >
+        <Button onClick={() => console.log(user)}>Log user</Button>
+        Calender + other components here
+      </Box>
+      <Box
+        borderColor="textPrimary"
+        sx={{
+          border: "2px solid",
+          gridColumn: "span 1",
+          padding: "10px",
+        }}
+      >
+        {user !== undefined ? <div>Hej {user.names.givenName}</div> : ""}
+        {user !== undefined ? <TasksList token={googleApiToken} /> : ""}
+      </Box>
+      <Box
+        borderColor="textPrimary"
+        sx={{
+          border: "2px solid",
+          gridColumn: "span 1",
+          padding: "10px",
+        }}
+      >
+        <ul>
+          <li>TODOS</li>
+          <li>Buss tabell</li>
+          <li>Annat?</li>
+        </ul>
+      </Box>
+    </>
+  );
 }
