b7155df0fb3408532df2f86072277dd36f49072d
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2022-09-13 16:28:01 +0200

    UseCurrentLocation.js is now functioning. Fetching location from user IP-address.

--- src/components/UseCurrentLocation.js
+++ src/components/UseCurrentLocation.js
@@ -1,27 +1,16 @@
-import { useState } from "react";
-
 //TODO Place this in folder hooks/utilisis?
 const UseCurrentLocation = async () => {
   let error = "";
   let location = "";
 
-  const handleSuccess = (position) => {
-    console.log(position);
-    location = position.coords;
-    console.log(location);
-    return location;
-  };
   const handleError = (error) => {
     console.log(error);
-    error = error.message;
     return error;
   };
 
   function getPosition() {
     return new Promise((resolve) =>
-      navigator.geolocation.getCurrentPosition(resolve, (err) => {
-        throw err;
-      })
+      navigator.geolocation.getCurrentPosition(resolve, handleError)
     );
   }
 
--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -1,35 +1,39 @@
-import { SettingsVoice } from "@mui/icons-material";
 import React from "react";
 import { useEffect, useState } from "react";
 import FetchData from "./FetchData";
 import UseCurrentLocation from "./UseCurrentLocation";
-
-/* const geolocationOptions = {
-  // this option defines when should the location request timeout
-  timeout: 1000 * 60 * 1, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
-}; */
+import Tooltip from "@mui/material/Tooltip";
 
 const WeatherWidget = () => {
   const [lat, setLat] = useState("59.32932349999999");
   const [long, setLong] = useState("18.068580800000063");
   const [data, setData] = useState([]);
-  let location;
   
     useEffect(() => {
       fetchDataWithLocation();
-    },[location]);
+    },[lat, long]);
 
 
    const fetchDataWithLocation = async () =>{
-      await getLocation();
-      console.log(location.coords.latitude);
-      await setCoordinates(location.latitude, location.longitude);
-      console.log(lat)
-      fetchWeatherData();
+    
+      const result = await getLocation();
+      if(result === false){
+      fetchWeatherData()
+       return;
+      }
+      console.log(result.coords.latitude);
+      setCoordinates(result.coords.latitude, result.coords.longitude).then(fetchWeatherData);
    }
 
    const getLocation = async () => {
-    location = await UseCurrentLocation();
+    
+    const result = await UseCurrentLocation();
+    
+    if(result.message === undefined){
+      return result;
+    }
+    console.log(result)
+    return false;
   
   }
   async function setCoordinates (latitude, longitude) {
@@ -42,31 +46,27 @@ const WeatherWidget = () => {
   const apiKey= "5622e7863c25a93700490cfec8116633"
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
   const fetchedData = await FetchData(url);
-  /* console.log(fetchedData) */
   setData(fetchedData);
  }
 
- /* const setCoordinates = (latitude, longitude) => {
-  setLat (latitude); //Tänk på att setFunktioner är asynkrona varför vi måste lyfta ut dessa i en egen funktion för att garantera att de har blivit satta innan url:en skickas iväg med parametrarna
-  setLong(longitude);
-  
- } */
   return(
-    
+    <Tooltip title={data[0].name}>
       <div className="weather-widget">
+        {console.log(data)}
         {data && 
           data.map(({weather, main, id}) => (
             <div key={id} className="flex-column">
              <div className="flex-row">
               <p style={{marginRight:"5px"}}>{Math.round(main.temp)} &#8451;</p>
               <img alt="weather icon" src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}/> 
-
               </div>
               <p style={{marginTop:"0"}}>{`${weather[0].description}`.charAt(0).toUpperCase() + `${weather[0].description}`.slice(1)}</p>
               
             </div>
           ))}  
+          
       </div>
+      </Tooltip>
 
   )
 }
