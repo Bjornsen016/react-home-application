d1ca9774333e1e2358767ef234715438b12ee922
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2022-09-19 16:01:03 +0200

    needs feedback on the TODOs

--- src/components/FactOfTheDay/FactOfTheDay.jsx
+++ src/components/FactOfTheDay/FactOfTheDay.jsx
@@ -1,16 +1,23 @@
 import FetchData from "../utils/FetchData";
 import { daysOfTheYearApiInfo } from "../../config/daysOfTheYearApiInfo";
 import { React, useState, useEffect } from "react";
+
 import Carousel from "react-material-ui-carousel";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import UpdateOutlinedIcon from "@mui/icons-material/UpdateOutlined";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export default function FactOfTheDay() {
   const day = new Date().getDate();
-  const month = new Date().getMonth() + 1; //TODO varför +1?
+  const month = new Date().getMonth() + 1;
   const year = new Date().getFullYear();
 
+  const [historyCard, setHistoryCard] = useState([]);
+  const [nameCard, setNameCard] = useState([]);
+  const [jokeCard, setJokeCard] = useState([]);
+  const [eventCard, setEventCard] = useState([]);
   const [cardInfo, setCardInfo] = useState([]);
 
   useEffect(() => {
@@ -23,7 +30,9 @@ export default function FactOfTheDay() {
         secondaryHeader: `History of this day`,
         primaryHeader: `Year ${historyData[0].events[index].year}`,
         body: `${historyData[0].events[index].description}`,
+        showbutton: true,
       };
+      setHistoryCard(historyCard);
       setCardInfo((prevAppCard) => [...prevAppCard, historyCard]);
     };
     const fetchJoke = async () => {
@@ -40,27 +49,29 @@ export default function FactOfTheDay() {
               setup: `${data[0].setup}\n\n`,
               response: `${data[0].delivery}`,
             };
-      const jokeAppCard = {
+      const jokeCard = {
         secondaryHeader: "Joke of the day",
         primaryHeader: "",
         body: `${jokeData.setup} ${jokeData.response} `,
       };
-
-      setCardInfo((prevAppCard) => [...prevAppCard, jokeAppCard]);
+      setJokeCard(jokeCard);
+      setCardInfo((prevAppCard) => [...prevAppCard, jokeCard]);
     };
     const fetchName = async () => {
       let nameData = await FetchData(
         `https://sholiday.faboul.se/dagar/v2.1/${year}/${month}/${day}`
       );
       nameData = nameData[0].dagar.map((arr) => arr.namnsdag.join(" och "));
-      const nameAppCard = {
+      const nameCard = {
         secondaryHeader: "Name of this day",
         primaryHeader: `${nameData} `,
         body: "",
       };
-      setCardInfo((prevAppCard) => [...prevAppCard, nameAppCard]);
+      setNameCard(nameCard);
+      setCardInfo((prevAppCard) => [...prevAppCard, nameCard]);
     };
     const fetchEvent = async () => {
+      //TODO needs to fetch via proxy for now and has to go if not fixed
       const headers = new Headers({
         "X-Api-Key": daysOfTheYearApiInfo.key,
       });
@@ -74,24 +85,67 @@ export default function FactOfTheDay() {
             primaryHeader: `${data.data[0].name}`,
             body: `${data.data[0].excerpt}`,
           };
+          setEventCard(eventCard);
           setCardInfo((prevAppCard) => [...prevAppCard, eventCard]);
         });
     };
-
     setCardInfo([]);
     fetchEvent();
     fetchHistory();
     fetchJoke();
     fetchName();
   }, []);
+  //TODO keep this update function?
+  const updateHistoryCard = () => {
+    const fetchHistory = async () => {
+      const historyData = await FetchData(
+        `https://byabbe.se/on-this-day/${month}/${day}/events.json`
+      );
+      const index = Math.floor(Math.random() * historyData[0].events.length);
+      const historyCard = {
+        secondaryHeader: `History of this day`,
+        primaryHeader: `Year ${historyData[0].events[index].year}`,
+        body: `${historyData[0].events[index].description}`,
+        showbutton: true,
+      };
+      setHistoryCard(historyCard);
+      setCardInfo((prevAppCard) => [...prevAppCard, historyCard]);
+    };
+    setCardInfo([]);
+    setCardInfo((prevAppCard) => [...prevAppCard, eventCard]);
+    setCardInfo((prevAppCard) => [...prevAppCard, jokeCard]);
+    fetchHistory();
+    setCardInfo((prevAppCard) => [...prevAppCard, nameCard]);
+  };
 
   function AppCard(props) {
     return (
       <Card>
         <CardContent>
           {" "}
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Typography
+            sx={{
+              fontSize: 14,
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+            color="text.secondary"
+            gutterBottom
+          >
             {props.item.secondaryHeader}
+            {props.item.showbutton ? (
+              <Button
+                //TODO buttonarea is not 100% clickable
+                size="small"
+                onClick={() => {
+                  updateHistoryCard();
+                }}
+              >
+                <UpdateOutlinedIcon />
+              </Button>
+            ) : (
+              ""
+            )}
           </Typography>
           <Typography variant="h6" component="div">
             {props.item.primaryHeader}
@@ -117,16 +171,16 @@ export default function FactOfTheDay() {
     <Carousel
       interval={8000}
       //TODO keep 200px or change to REM?
-      height={200}
+      height={220}
       indicatorIconButtonProps={{
         style: {
-          color: "blue",
+          color: "",
           //TODO change colors or use deafult?
         },
       }}
       activeIndicatorIconButtonProps={{
         style: {
-          backgroundColor: "red",
+          backgroundColor: "",
           //TODO change colors or use deafult?
         },
       }}
