763066d26b85ecf693ebc05dc62c85baae9c1742
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2022-09-20 23:15:56 +0200

    Proposal responsive TopBar-design

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -8,52 +8,59 @@ import LoginWithGoogle from "./LoginWithGoogle";
 //TODO: Make this into more smaller components
 //TODO: Refactor to have a better understanding of what happens
 export default function TopBar({
-	colorMode,
-	user,
-	setGoogleApiToken,
-	setUser,
-	setChosenCalendars,
+  colorMode,
+  user,
+  setGoogleApiToken,
+  setUser,
+  setChosenCalendars,
 }) {
-	return (
-		<AppBar position='sticky' color='primary'>
-			<Toolbar
-				sx={{
-					display: "grid",
-					justifyContent: "space-around",
-					gridTemplateColumns: "33% 33% 33%",
-					justifyItems: "center",
-				}}
-			>
-				<Box
-					sx={{ display: "flex", alignItems: "center", justifySelf: "start" }}
-				>
-					<TopBarMenu
-						user={user}
-						setGoogleApiToken={setGoogleApiToken}
-						setUser={setUser}
-						setChosenCalendars={setChosenCalendars}
-					/>
-					<Checkbox
-						onChange={colorMode.toggleColorMode}
-						icon={<LightMode sx={{ color: "yellow" }} />}
-						checkedIcon={<DarkMode sx={{ color: "white" }} />}
-					/>
-					<Typography variant='h6'>{user?.names.displayName}</Typography>
-					{!user && (
-						<LoginWithGoogle
-							setGoogleApiToken={setGoogleApiToken}
-							setUser={setUser}
-						/>
-					)}
-				</Box>
-				<Clock />
-				{/* TODO: Make center on the top everytime, make instead of flex? */}
-				<Box sx={{ justifySelf: "end" }}>
-					<Typography variant='p'>
-						<WeatherWidget />
-					</Typography>
-				</Box>
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar position="sticky" color="primary">
+      <Toolbar
+        sx={{
+          display: "grid",
+          justifyContent: "space-around",
+          gridTemplateColumns: "33% 34% 33%",
+          justifyItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifySelf: "start",
+            flexWrap: "wrap",
+          }}
+        >
+          <TopBarMenu
+            user={user}
+            setGoogleApiToken={setGoogleApiToken}
+            setUser={setUser}
+            setChosenCalendars={setChosenCalendars}
+          />
+          <Checkbox
+            onChange={colorMode.toggleColorMode}
+            icon={<LightMode sx={{ color: "yellow" }} />}
+            checkedIcon={<DarkMode sx={{ color: "white" }} />}
+          />
+          <Typography variant="h6" sx={{ fontSize: { xs: "80%", sm: "100%" } }}>
+            {user?.names.displayName}
+          </Typography>
+          {!user && (
+            <LoginWithGoogle
+              setGoogleApiToken={setGoogleApiToken}
+              setUser={setUser}
+            />
+          )}
+        </Box>
+        <Clock />
+        {/* TODO: Make center on the top everytime, make instead of flex? */}
+        <Box sx={{ justifySelf: "end", fontSize: { xs: "80%", sm: "100%" } }}>
+          <Typography variant="p">
+            <WeatherWidget />
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
