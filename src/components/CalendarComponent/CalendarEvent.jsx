97d0a33e36b3e3c744371d4e8b6b967008ea695a
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2022-09-20 09:57:30 +0200

    Started some design on calender component. Added black weather icons.

--- src/components/CalendarComponent/CalendarEvent.jsx
+++ src/components/CalendarComponent/CalendarEvent.jsx
@@ -1,18 +1,29 @@
 import { Divider, Typography } from "@mui/material";
-export const CalendarEvent = ({ event }) => {
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-			}}
-		>
-			<Typography sx={{ width: "90%" }} align='center' variant='h5' noWrap>
-				{event.title}
-			</Typography>
-			<Typography variant='h6'>{event.time}</Typography>
-			<Divider sx={{ width: "50%" }} />
-		</div>
-	);
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+
+export const CalendarEvent = ({ event, index }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: "10px",
+      }}
+    >
+      <ListItemAvatar>
+        <Avatar sx={{ color: "primary" }}>{index + 1}</Avatar>
+      </ListItemAvatar>
+      <div>
+        <Typography sx={{ width: "90%" }} align="left" variant="h5" noWrap>
+          {event.title}
+        </Typography>
+        <Typography sx={{ width: "90%" }} align="left" variant="p">
+          {event.time}
+        </Typography>
+        <Divider sx={{ width: "90%" }} style={{ marginTop: "5px" }} />
+      </div>
+    </div>
+  );
 };
