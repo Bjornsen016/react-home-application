a6c8b821e6db175e518b89516fbb38afc368e085
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-09-28 15:12:22 +0200

    Now using firebase. Able to refresh tokens. Saving them improperly right now, gotta fix that

--- src/config/googleApiInfo.js
+++ src/config/googleApiInfo.js
@@ -9,4 +9,33 @@ export const googleApiInfo = {
 		"https://people.googleapis.com/v1/people/me?personFields=names,photos",
 	scopes:
 		"https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/tasks",
+	googleAuthLinkGen: async () => {
+		const data = await fetch(
+			`https://homehubserverhandler.bjornsen016.repl.co/authLink`,
+			{
+				method: "POST",
+				header: { "Content-Type": "application/json" },
+			}
+		).then((res) => res.json());
+
+		console.log("data: ", data);
+		window.location.replace(data.authLink);
+	},
+	googleRenewAuthToken: async (refreshToken) => {
+		const newBody = { refresh_token: refreshToken };
+
+		const headers = new Headers({
+			"Content-Type": "application/json;charset=utf-8",
+		});
+		const data = await fetch(
+			`https://homehubserverhandler.bjornsen016.repl.co/getNewAccesToken`,
+			{
+				method: "POST",
+				body: JSON.stringify(newBody),
+				headers: headers,
+			}
+		).then((res) => res.json());
+
+		return data.token;
+	},
 };
