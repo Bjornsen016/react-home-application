fda39808e2e5b7ddf5da760d2f314f2eb2516ac2
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2022-10-15 16:27:32 +0200

    working draft for feching news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,11 +1,39 @@
 import { genereteNewsUrl } from "../../config/newsApiInfo";
 import { React, useState, useEffect } from "react";
-import { Card, CardContent, Button, Typography } from "@mui/material";
+import { Card, CardContent, Typography, Link } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 
 export default function News() {
-  const [newsData, setNewsData] = useState([]);
-  const [newsCard, setNewsCard] = useState([]);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const url = genereteNewsUrl();
+      const newsData = await fetchData(url);
+      console.log(newsData);
+      newsData.articles.forEach((article) => {
+        const newsCard = {
+          secondaryHeader: `News of the day`,
+          primaryHeader: `${article.source.name}`,
+          body: `${article.title}`,
+          link: () => (
+            <Link
+              target="_blank"
+              href={article.url}
+              sx={{
+                color: "text.primary",
+              }}
+            >
+              Read more
+            </Link>
+          ),
+        };
+        setArticles((prevAppCard) => [...prevAppCard, newsCard]);
+      });
+    };
+    setArticles([]);
+    fetchNews();
+  }, []);
 
   async function fetchData(url) {
     const data = await fetch(url)
@@ -15,77 +43,61 @@ export default function News() {
     return data;
   }
 
-  function caruselCard(props) {
-    return (
-      <Card>
-        <CardContent
-          sx={{
-            backgroundColor: "inherit",
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: 14,
-              display: "flex",
-              justifyContent: "space-between",
-              textTransform: "uppercase",
-              color: "primary.main",
-            }}
-            gutterBottom
-          >
-            {newsCard.secondaryHeader}
-          </Typography>
-          <Typography
-            sx={{
-              marginBottom: "0.35em",
-            }}
-            variant="h6"
-            component="div"
-          >
-            {newsCard.primaryHeader}
-          </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              display: "flex",
-              height: "100px",
-              overflow: "auto",
-              whiteSpace: "pre-line",
-            }}
-          >
-            {newsCard.body}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-  }
-  useEffect(() => {
-    const fetchNews = async () => {
-      const url = genereteNewsUrl();
-      const newsData = await fetchData(url);
-      setNewsData(newsData);
-      console.log(newsData);
-      const newsCard = {
-        secondaryHeader: `Todays news`,
-        primaryHeader: `${newsData.articles[14].author}`,
-        body: `${newsData.articles[14].title}`,
-        showbutton: false,
-      };
-      setNewsCard(newsCard);
-    };
-
-    fetchNews();
-  }, []);
-
   return (
     <Carousel
       className="carousel"
       sx={{ overflow: { xs: "visible" }, height: { sm: "85%" } }}
       interval={8000}
     >
-      {newsCard?.map((item, i) =>
-        item.body ? <caruselCard key={i} item={item} /> : ""
-      )}
+      {articles.map((item, i) => (
+        <AppCard key={i} item={item} />
+      ))}
     </Carousel>
+  );
+}
+
+function AppCard(props) {
+  return (
+    <Card>
+      <CardContent
+        sx={{
+          backgroundColor: "inherit",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: 14,
+            display: "flex",
+            justifyContent: "space-between",
+            textTransform: "uppercase",
+            color: "primary.main",
+          }}
+          gutterBottom
+        >
+          {props.item.secondaryHeader}
+        </Typography>
+        <Typography
+          sx={{
+            marginBottom: "0.35em",
+          }}
+          variant="h6"
+          component="div"
+        >
+          {props.item.primaryHeader}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            display: "flex",
+            height: "100px",
+            overflow: "auto",
+            whiteSpace: "pre-line",
+          }}
+        >
+          {props.item.body}
+        </Typography>
+        <Typography>{props.item.link()}</Typography>
+      </CardContent>
+    </Card>
   );
 }
